342fc839f66c609a2e6dbf5fd1569ff0edade196
jinnurain-jihan <zinnurainzihan171@gmail.com>
2022-11-09 20:10:59 +0600

    javascript

---
+++ app.js
@@ -0,0 +1,11 @@
+var name=prompt("What is your name?");
+var food=prompt("what is favourite food?");
+
+console.log("my name is "+name+" & i love to eat "+food+",bergur.");
+
+
+
+
+console.log("Sajid"+" shohan"+" Jihan"+" Siam");
+
+
